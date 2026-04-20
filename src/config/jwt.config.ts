16ae73e6c66e265dd5d70b3f69b711840cc4452a
vishnu prasath <vishnuprasath.s@calibraint.com>
2026-04-20 21:17:04 +0530

    fix: debugging the deployment issue

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,8 +1,11 @@
-import { registerAs } from '@nestjs/config';
+import { registerAs } from "@nestjs/config";
 
-export default registerAs('jwt', () => ({
-  accessSecret: process.env.JWT_ACCESS_SECRET,
-  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? '15m',
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? '7d',
-}));
+export default registerAs("jwt", () => {
+  console.log("JWT_ACCESS_SECRET exists:", !!process.env.JWT_ACCESS_SECRET);
+  return {
+    accessSecret: process.env.JWT_ACCESS_SECRET,
+    accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN ?? "15m",
+    refreshSecret: process.env.JWT_REFRESH_SECRET,
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN ?? "7d",
+  };
+});
